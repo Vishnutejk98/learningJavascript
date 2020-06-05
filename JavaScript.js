f7341607518315e476c30d224421acdbeff72709
vishnutejk98 <mailboxnumber04@gmail.com>
2020-06-05 22:00:24 +0530

    Javascript

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+//Comments 
+//In line comment 
+/*Multi line Comment */
+
+/* They are 7 datatypes in the javascript
+null, bollean, string, number, undefined, symbol and object */
+
+//Declaring the variable
+var variableA;
+let variableB;
+const variableC = 10;
+
+//Assigning and declaring variable
+var a = 10;
+var b = 20;
+
+//Basic arthemetic 
+console.log("Addtion of two numbers : "+(a+b));
+console.log("Subtraction of two numbers : "+(a-b));
+console.log("Multiplication of two numbers : "+(a*b));
+console.log("Division of two numbers : "+(a/b));
+console.log("Increment Operation a++ : "+(a++));
+console.log("Decrement Operation a-- : "+(a--));
